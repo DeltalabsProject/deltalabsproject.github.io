d36657fbb19dbaf966559657a6f628a32d19e2c5
DeltalabsProject <deltalabsproject@gmail.com>
2017-02-04 05:42:05 +0700

    Add files via upload

---
+++ deltabbm/apps/script.js
@@ -0,0 +1,45 @@
+/*! Copyright © 2016 / FD:FRS
+*
+* http://fdfrs.org
+*
+*/ 
+$(document).ready(function() {
+  $('.wave').fdfrs_rippleEffect();
+});
+(function($) {
+  $.fn.fdfrs_rippleEffect = function() {
+    var btn, self, ripple, size, rippleX, rippleY, eWidth, eHeight;
+    btn = $(this).not('[disabled], .disabled');
+    btn.on('mousedown', function(e) {
+      self = $(this);
+      if(e.button === 2) {
+        return false;
+      }
+      if(self.find('.ripple').length === 0) {
+        self.prepend('<span class="ripple"></span>');
+      }
+      ripple = self.find('.ripple');
+      ripple.removeClass('animated');
+
+      eWidth = self.outerWidth();
+      eHeight = self.outerHeight();
+      size = Math.max(eWidth, eHeight);
+      ripple.css({'width': size, 'height': size});
+
+      rippleX = parseInt(e.pageX - self.offset().left) - (size / 2);
+      rippleY = parseInt(e.pageY - self.offset().top) - (size / 2);
+
+      ripple.css({ 'top': rippleY +'px', 'left': rippleX +'px' }).addClass('animated');
+
+      setTimeout(function() {
+        ripple.remove();
+      }, 800);
+
+    });
+  };
+}(jQuery));
+/*! Copyright © 2016 / FD:FRS
+*
+* http://fdfrs.org
+*
+*/ 
